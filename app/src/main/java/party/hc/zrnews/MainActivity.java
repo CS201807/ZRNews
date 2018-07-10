b2a55e66243d74012608b531041e3c2b722c10fa
bian <wolianlxw@sina.com>
2018-07-10 17:18:13 +0800

    Initial commit

---
+++ app/src/main/java/party/hc/zrnews/MainActivity.java
@@ -0,0 +1,13 @@
+package party.hc.zrnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
